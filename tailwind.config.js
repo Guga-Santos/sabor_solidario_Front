274f5d33e893750831ecaec08b6a5582228d85d5
Manuele Ferreira <maanueleferreira@gmail.com>
2024-10-21 22:54:05 -0300

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,15 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+      'text-bro': '#5D576B',
+      'second-green': '#99E1D9',
+      'second-pink': '#F7567C',
+      'second-yellow': '#FFFAE3',
+      'second-gray': '#FCFCFC',
+      'text-gray': '#7A7786',
+      'second-gray': '#4E4B59',
+      'tercery-gray': '#33303E',
+      'opacity-gray': 'rgba(100, 80, 57, 0.1)',
     },
   },
   plugins: [],
