ea7ee0b5088592abd7ce62130eaf31a5d85feeef
Manuele Ferreira <maanueleferreira@gmail.com>
2024-10-28 17:09:44 -0300

    alterações ui tela de cadastro voluntários, implementação tag Link nas páginas para otimização das rotas.

--- src/app/cadastroRestaurante/page.jsx
+++ src/app/cadastroRestaurante/page.jsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Link from 'next/link'
 import Image from "next/image";
 import logo from '../../assets/login-login-teste.svg'
 import voltar from '../../assets/botao-voltar.svg'
@@ -10,9 +11,9 @@ export default function CadastroRestaurante() {
         <div className="flex flex-col w-[40%] justify-evenly items-center py-4 bg-second-yellow text-center">
             <div className="absolute left-7 top-8 w-7"><a><Image src={voltar} alt="botão Voltar" /></a></div>
 
-            <a href="/">
+            <Link href="/">
                 <Image className="w-[150px]" src={logo} alt="Logo" />
-            </a>
+            </Link>
 
             <div className="font-extrabold text-[50px] px-10  leading-[1]"> Preencha os dados e cadastre-se!</div>
 
@@ -53,102 +54,108 @@ export default function CadastroRestaurante() {
                 <div className="font-extrabold text-second-green text-[40px] ">Faça o seu cadastro agora</div>
                 <div> <span className="font-bold">Ao lado</span> você encontra instruções em caso de dúvida.</div>
             </div>
-            
+
             {/* Inputs - Dados empresariais */}
             <div className="items-start px-10">
                 <p className="font-bold text-second-green">Dados empresariais</p>
-                <ul className="grid grid-cols-1 md:grid-cols-3 gap-2 mx-6">
-                    <li className="flex flex-col ">
-                        <label>Digite a Razão Social *</label>
-                        <input className="flex h-10 border-solid border-2 border-text-bro rounded-md outline-none p-4 focus:border-second-green text-xs" id="razaoSocial" type="text" placeholder="Digite aqui sua Razão Social" />
-                    </li>
-
-                    <li className="flex flex-col ">
-                        <label>Digite o Nome Fantasia *</label>
-                        <input className="flex h-10 border-solid border-2 border-text-bro rounded-md outline-none p-4 focus:border-second-green text-xs" id="nomeFantasia" type="text" placeholder="Digite aqui o Nome Fantansia" />
-                    </li>
-
-                    <div>
-                        <li className="flex flex-col w-[90%]">
-                            <label>Anexe sua imagem de perfil *</label>
-                            <input className="flex border-solid border-2 border-text-bro rounded-md outline-none p-4 focus:border-second-green text-xs" id="imagemUser" accept="image/*" type="file" />
+                <form>
+                    <ul className="grid grid-cols-1 md:grid-cols-3 gap-2 mx-6">
+                        <li className="flex flex-col ">
+                            <label>Digite a Razão Social *</label>
+                            <input className="flex h-10  bg-slate-100 border-s-4 rounded-md outline-none p-4 focus:border-second-green text-xs" id="razaoSocial" type="text" placeholder="Digite aqui sua Razão Social" />
                         </li>
-                    </div>
 
-                    <li className="flex flex-col ">
-                        <label>Digite seu CNPJ *</label>
-                        <input className="flex h-10 border-solid border-2 border-text-bro rounded-md outline-none p-4 focus:border-second-green text-xs" id="cnpj" type="text" placeholder="Digite aqui o CNPJ" />
-                    </li>
+                        <li className="flex flex-col ">
+                            <label>Digite o Nome Fantasia *</label>
+                            <input className="flex h-10  bg-slate-100 border-s-4 rounded-md outline-none p-4 focus:border-second-green text-xs" id="nomeFantasia" type="text" placeholder="Digite aqui o Nome Fantansia" />
+                        </li>
 
-                    <li className="flex flex-col ">
-                        <label>Digite o telefone *</label>
-                        <input className="flex h-10 border-solid border-2 border-text-bro rounded-md outline-none p-4 focus:border-second-green text-xs" id="telefone" type="number" placeholder="Digite aqui seu telefone" />
-                    </li>
+                        <div>
+                            <li className="flex flex-col w-[90%]">
+                                <label>Anexe a imagem de perfil *</label>
+                                <input className="flex  bg-slate-100 border-s-4 rounded-md outline-none p-4 focus:border-second-green text-xs" id="imagemUser" accept="image/*" type="file" />
+                            </li>
+                        </div>
 
+                        <li className="flex flex-col ">
+                            <label>Digite o CNPJ *</label>
+                            <input className="flex h-10  bg-slate-100 border-s-4 rounded-md outline-none p-4 focus:border-second-green text-xs" id="cnpj" type="text" placeholder="Digite aqui o CNPJ" />
+                        </li>
 
-                </ul>
+                        <li className="flex flex-col ">
+                            <label>Digite o telefone *</label>
+                            <input className="flex h-10  bg-slate-100 border-s-4 rounded-md outline-none p-4 focus:border-second-green text-xs" id="telefone" type="number" placeholder="Digite aqui seu telefone" />
+                        </li>
+                    </ul>
+                </form>
+
             </div>
 
             {/* Inputs - Endereço */}
             <div className="items-start px-10 ">
                 <p className="font-bold text-second-green">Endereço</p>
-                <ul className="grid grid-cols-1 md:grid-cols-3 gap-2 mx-6">
-                    <li className="flex flex-col ">
-                        <label>Digite o nome da Rua *</label>
-                        <input className="flex h-10 border-solid border-2 border-text-bro rounded-md outline-none p-4 focus:border-second-green text-xs" id="nomeRua" type="text" placeholder="Digite aqui sua rua" />
-                    </li>
+                <form>
+                    <ul className="grid grid-cols-1 md:grid-cols-3 gap-2 mx-6">
+                        <li className="flex flex-col ">
+                            <label>Digite o nome da Rua *</label>
+                            <input className="flex h-10  bg-slate-100 border-s-4 rounded-md outline-none p-4 focus:border-second-green text-xs" id="nomeRua" type="text" placeholder="Digite aqui sua rua" />
+                        </li>
 
-                    <li className="flex flex-col ">
-                        <label>Digite o número *</label>
-                        <input className="flex h-10 border-solid border-2 border-text-bro rounded-md outline-none p-4 focus:border-second-green text-xs" id="numeroRua" type="number" placeholder="Digite aqui o número da sua residência" />
-                    </li>
+                        <li className="flex flex-col ">
+                            <label>Digite o número *</label>
+                            <input className="flex h-10  bg-slate-100 border-s-4 rounded-md outline-none p-4 focus:border-second-green text-xs" id="numeroRua" type="number" placeholder="Digite aqui o número da sua residência" />
+                        </li>
 
-                    <li className="flex flex-col ">
-                        <label>Digite o complemento</label>
-                        <input className="flex h-10 border-solid border-2 border-text-bro rounded-md outline-none p-4 focus:border-second-green text-xs" id="complemento" type="text" placeholder="Digite aqui o complemento" />
-                    </li>
+                        <li className="flex flex-col ">
+                            <label>Digite o complemento</label>
+                            <input className="flex h-10 bg-slate-100 border-s-4 rounded-md outline-none p-4 focus:border-second-green text-xs" id="complemento" type="text" placeholder="Digite aqui o complemento" />
+                        </li>
 
-                    <li className="flex flex-col ">
-                        <label>Digite o Bairro *</label>
-                        <input className="flex h-10 border-solid border-2 border-text-bro rounded-md outline-none p-4 focus:border-second-green text-xs" id="bairro" type="text" placeholder="Digite aqui seu bairro" />
-                    </li>
+                        <li className="flex flex-col ">
+                            <label>Digite o Bairro *</label>
+                            <input className="flex h-10 bg-slate-100 border-s-4 rounded-md outline-none p-4 focus:border-second-green text-xs" id="bairro" type="text" placeholder="Digite aqui seu bairro" />
+                        </li>
 
-                    <li className="flex flex-col ">
-                        <label>Digite sua cidade *</label>
-                        <input className="flex h-10 border-solid border-2 border-text-bro rounded-md outline-none p-4 focus:border-second-green text-xs" id="cidade" type="text" placeholder="Digite aqui a cidade" />
-                    </li>
+                        <li className="flex flex-col ">
+                            <label>Digite a cidade *</label>
+                            <input className="flex h-10 bg-slate-100 border-s-4 rounded-md outline-none p-4 focus:border-second-green text-xs" id="cidade" type="text" placeholder="Digite aqui a cidade" />
+                        </li>
 
-                    <li className="flex flex-col ">
-                        <label>Digite seu CEP *</label>
-                        <input className="flex h-10 border-solid border-2 border-text-bro rounded-md outline-none p-4 focus:border-second-green text-xs" id="cep" type="number" placeholder="Digite aqui o CEP" />
-                    </li>
-                </ul>
+                        <li className="flex flex-col ">
+                            <label>Digite o CEP *</label>
+                            <input className="flex h-10  bg-slate-100 border-s-4 rounded-md outline-none p-4 focus:border-second-green text-xs" id="cep" type="number" placeholder="Digite aqui o CEP" />
+                        </li>
+                    </ul>
+                </form>
             </div>
 
             {/* Inputs - Acesso */}
             <div className="items-start px-10 ">
                 <p className="font-bold text-second-green">Dados de acesso</p>
-                <ul className="grid grid-cols-1 md:grid-cols-2 gap-2 mx-6">
-                    <li className="flex flex-col ">
-                        <label>Digite seu endereço de e-mail *</label>
-                        <input className="flex h-10 border-solid border-2 border-text-bro rounded-md outline-none p-4 focus:border-second-green text-xs" id="emailCadastro" type="text" placeholder="Digite aqui seu e-mail" />
-                    </li>
+                <form>
+                    <ul className="grid grid-cols-1 md:grid-cols-2 gap-2 mx-6">
+                        <li className="flex flex-col ">
+                            <label>Digite seu e-mail *</label>
+                            <input className="flex h-10  bg-slate-100 border-s-4 rounded-md outline-none p-4 focus:border-second-green text-xs" id="emailCadastro" type="text" placeholder="Digite aqui seu e-mail" />
+                        </li>
 
-                    <li className="flex flex-col ">
-                        <label>Confirme seu endereço de e-mail *</label>
-                        <input className="flex h-10 border-solid border-2 border-text-bro rounded-md outline-none p-4 focus:border-second-green text-xs" id="confirmacaoEmailCadastro" type="text" placeholder="Confirme aqui seu e-mail" />
-                    </li>
+                        <li className="flex flex-col ">
+                            <label>Confirme o e-mail *</label>
+                            <input className="flex h-10  bg-slate-100 border-s-4 rounded-md outline-none p-4 focus:border-second-green text-xs" id="confirmacaoEmailCadastro" type="text" placeholder="Confirme aqui seu e-mail" />
+                        </li>
 
-                    <li className="flex flex-col ">
-                        <label>Digite sua senha *</label>
-                        <input className="flex h-10 border-solid border-2 border-text-bro rounded-md outline-none p-4 focus:border-second-green text-xs" id="senha" type="number" placeholder="Digite aqui sua senha" />
-                    </li>
+                        <li className="flex flex-col ">
+                            <label>Digite a senha *</label>
+                            <input className="flex h-10  bg-slate-100 border-s-4 rounded-md outline-none p-4 focus:border-second-green text-xs" id="senha" type="password" placeholder="Digite aqui sua senha" />
+                        </li>
 
-                    <li className="flex flex-col ">
-                        <label>Confirme sua senha *</label>
-                        <input className="flex h-10 border-solid border-2 border-text-bro rounded-md outline-none p-4 focus:border-second-green text-xs" id="confirmacaoSenhaCadastro" type="number" placeholder="Confirme aqui sua senha" />
-                    </li>
-                </ul>
+                        <li className="flex flex-col ">
+                            <label>Confirme a senha *</label>
+                            <input className="flex h-10  bg-slate-100 border-s-4 rounded-md outline-none p-4 focus:border-second-green text-xs" id="confirmacaoSenhaCadastro" type="number" placeholder="Confirme aqui sua senha" />
+                        </li>
+                    </ul>
+                </form>
+
             </div>
         </div>
     </div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,8 @@
-import Image from "next/image";
+import Image from "next/image"
+import Link from 'next/link'
 import logo from '../assets/logo svg.svg'
-import seta from  '../assets/seta ri.svg'
-import hero from  '../assets/bg-heeerosectionsvg.svg'
+import seta from '../assets/seta ri.svg'
+import hero from '../assets/bg-heeerosectionsvg.svg'
 import imgvolunt from '../assets/Handmade.svg'
 import imgrestaur from '../assets/Small Business.svg'
 import setasimples from '../assets/Right.svg'
@@ -10,83 +11,96 @@ export default function Home() {
   return (
     <div className="container h-[100vh] w-[100vw] min-w-screen">
       {/* Header */}
-      <nav className="flex pt-4 justify-around items-center w-[100vw]">
-          <div>
-            <Image className="w-[106px]" src={logo} alt="Logo"/>
-          </div>
+      <nav className="flex pt-4 justify-around items-center w-[100vw] sticky top-0 bg-white">
+        <div>
+          <Link href='/'>
+            <Image className="w-[106px]" src={logo} alt="Logo" /> 
+          </Link>
+        </div>
 
-          <div>
-            <ul className="flex text-center gap-8 text-1xl font-semibold">
-              <li><a className="hover:text-second-pink transition" href="/"> Home </a></li>
-              <li><a className="hover:text-second-pink transition" href="#"> Sobre nós </a></li>
-              <li><a className="hover:text-second-pink transition" href="#"> Dúvidas </a></li>
-            </ul>
-          </div>
+        <div>
+          <ul className="flex text-center gap-8 text-1xl font-semibold">
+            <li><Link className="hover:text-second-pink transition" href="/"> Home </Link></li>
+            <li><Link className="hover:text-second-pink transition" href="/"> Sobre nós </Link></li>
+            <li><Link className="hover:text-second-pink transition" href="/"> Dúvidas </Link></li>
+          </ul>
+        </div>
 
-          <div className="flex gap-4 text-[14px]"> 
-          <a href="/cadastro"><button className="border-solid border-2 border-text-bro px-6 py-1 rounded-md font-semibold hover:scale-105 transition"> Cadastre-se</button></a>
-            <a href="/login"><button className="bg-second-pink px-14 py-1 rounded-md font-semibold text-second-yellow hover:scale-105 hover:bg-second-pink-hover transition	"> Entrar</button></a>
-          </div>
+        <div className="flex gap-4 text-[14px]">
+          <Link href="/cadastro">
+            <button className="border-solid border-2 border-text-bro px-6 py-1 rounded-md font-semibold hover:scale-105 transition">Cadastre-se</button>
+          </Link>
+          <Link href="/login">
+            <button className="bg-second-pink px-14 py-1 rounded-md font-semibold text-second-yellow hover:scale-105 hover:bg-second-pink-hover transition">Entrar</button>
+          </Link>
+        </div>
       </nav>
 
       {/* HeroSection */}
+      {/* ||||||||||||| AJUSTAR RESPONSIVIDADE TEXTO ||||||||||||| */}
       <div className="flex justify-center items-center w-[100vw] h-[80vh]">
-          <div className="flex flex-col flex-1 pl-60"> 
-            <h1 className="text-[80px] w-[100%] leading-[80px] font-bold  tracking-[-.03em]">
-              <span className="text-second-pink">Juntos</span>, podemos construir um futuro melhor<span className="text-second-pink">!</span>
-            </h1>
+        <div className="flex flex-col flex-1 pl-60">
+          <h1 className="text-[78px] w-[100%] leading-[80px] font-bold  tracking-[-.03em]">
+            <span className="text-second-pink">Juntos</span>, podemos construir um futuro melhor<span className="text-second-pink">!</span>
+          </h1>
 
-            <p className="font-medium text-[20px] mt-4">Sua doação nos ajuda a salvar o mundo.</p>
-            <p className="font-bold text-[20px]">ainda <span className="text-second-pink">não</span> tem cadastro?</p>
+          <p className="font-medium text-[20px] mt-4">Sua doação nos ajuda a salvar o mundo.</p>
+          <p className="font-bold text-[20px]">ainda <span className="text-second-pink">não</span> tem cadastro?</p>
 
-            {/* alterar efeito do hover */}
-            <a href="/cadastro">
-              <div className="flex w-[220] h-[50] items-center justify-center gap-4 border-solid border-2 border-second-pink rounded-[50px] font-bold hover:scale-105 hover:bg-second-pink hover:border-second-pink-hover hover:text-white transition mt-4">
+          {/* alterar efeito do hover */}
+          <Link href="/cadastro">
+            <div className="flex w-[220] h-[50] items-center justify-center gap-4 border-solid border-2 border-second-pink rounded-[50px] font-bold hover:scale-105 hover:bg-second-pink hover:border-second-pink-hover hover:text-white transition mt-4">
               <button> Cadastre-se </button>
-              <Image src={seta} />
-              </div>
-            </a>
-          </div>
+              <Image src={seta} alt="imagem voltar"/>
+            </div>
+          </Link>
+        </div>
 
-          <div className="flex flex-1 items-end mr-20">
-            <Image src={hero} />
-          </div>
+        <div className="flex flex-1 items-end mr-20">
+          <Image src={hero} alt="Imagem de frutas" />
+        </div>
       </div>
 
       {/* Section 2 - chamada cadastro */}
-      <div className="w-[100vw]"> 
-        <div className="flex items-center justify-center w-[100vw] text-[35px] gap-2 font-bold "> 
+      <div className="w-[100vw]">
+        <div className="flex items-center justify-center w-[100vw] text-[35px] gap-2 font-bold ">
           <p>Venha <span className="text-second-pink" >fazer parte</span> do nosso time</p>
-          <Image className="rotate-90" src={seta} />
+          <Image className="rotate-90" src={seta} alt="imagem seta"/>
         </div>
 
 
         <div className="w-[100vw] h-[35vh] flex pt-4 gap-2 rotate-20 ">
-           <div className="flex flex-1 bg-second-yellow items-center justify-center gap-5"> 
-            <Image src={imgvolunt} />
-            
-              <div className="flex flex-col items-start gap-2 w-[35%]"> 
-                <p className="font-bold text-[30px] leading-[35px]"> Junte-se ao nosso grupo de <span className="text-second-pink">voluntários</span>! </p>
-                <a href="/cadastroVoluntario"><button className=" flex justify-center items-center gap-2 border-solid border-2 border-text-bro px-6 py-1 rounded-md font-semibold hover:scale-105 transition"><a>Clica e faça parte </a>
-                <Image src={setasimples} /> </button> </a>
-              </div>
-          </div>
-          
-          <div className="flex flex-1 bg-second-yellow gap-2 justify-center items-center"> 
-            <div className="flex flex-col items-end gap-2 w-[40%] text-end justify-center"> 
-              <p className="font-bold text-[30px] leading-[35px]"> Seja uma das nossas <span className="text-second-pink">empresas</span> parceiras! </p>
-              <a href="/cadastroRestaurante"> <button className=" flex justify-center items-center gap-2 border-solid border-2 border-text-bro px-6 py-1 rounded-md font-semibold hover:scale-105 transition"><a>Clica e faça parte </a>
-              <Image src={setasimples} /> </button> </a>
+          <div className="flex flex-1 bg-second-yellow items-center justify-center gap-5">
+            <Image src={imgvolunt} alt="imagem mãos e um coração representando doação"/>
+
+            <div className="flex flex-col items-start gap-2 w-[35%]">
+              <p className="font-bold text-[30px] leading-[35px]"> Junte-se ao nosso grupo de <span className="text-second-pink">voluntários</span>! </p>
+              <Link href="/cadastroVoluntario">
+                <button className=" flex justify-center items-center gap-2 border-solid border-2 border-text-bro px-6 py-1 rounded-md font-semibold hover:scale-105 transition">Clica e faça parte
+                  <Image src={setasimples} alt="imagem seta do botão"/>
+                </button>
+              </Link>
             </div>
-            
-            <Image src={imgrestaur} />
+          </div>
+
+          <div className="flex flex-1 bg-second-yellow gap-2 justify-center items-center">
+            <div className="flex flex-col items-end gap-2 w-[40%] text-end justify-center">
+              <p className="font-bold text-[30px] leading-[35px]"> Seja uma das nossas <span className="text-second-pink">empresas</span> parceiras! </p>
+              <Link href="/cadastroRestaurante">
+                <button className=" flex justify-center items-center gap-2 border-solid border-2 border-text-bro px-6 py-1 rounded-md font-semibold hover:scale-105 transition">Clica e faça parte
+                  <Image src={setasimples} alt="imagem seta do botão"/>
+                </button>
+              </Link>
+            </div>
+
+            <Image src={imgrestaur} alt="imagem estabelecimento restaurante" />
           </div>
         </div>
       </div>
 
       {/* Section Sobre nós / contato / dúvidas */}
       <div className="w-[100vw] h-[60vh]">
-        
+
 
       </div>
 
@@ -96,9 +110,9 @@ export default function Home() {
 
       </div>
 
-    
+
 
     </div>
-    
+
   );
 }
